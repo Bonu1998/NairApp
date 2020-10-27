8dee32ec53cc59e10ac13e2ac884d1babc011ace
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-27 22:55:27 +0530

    added flashcard routes

--- routes/api/flashcard.js
+++ routes/api/flashcard.js
@@ -11,10 +11,10 @@ const validateFlashcardInput = require('../../validation/flashcard');
 
 // @desc    View all Flash cards
 // @route   GET /flashcard/
-router.get('/', /*ensureAuth,*/ (req, res)=>{
-	// User.findById(req.user.id)
-	// 	.then(user => {
-			FlashCard.find(/*{user: req.user.id}*/)
+router.get('/', ensureAuth, (req, res)=>{
+	User.findById(req.user.id)
+		.then(user => {
+			FlashCard.find({user: req.user.id})
 				.sort({date: -1})
 				.then(flashcards => {
 					if(!flashcards)
@@ -22,22 +22,38 @@ router.get('/', /*ensureAuth,*/ (req, res)=>{
 					res.status(200).json({success: true, message: 'here are all your flashcards', flashcards});
 				})
 				.catch(err => res.status(500).json({success: false, message: 'Unable to fetch your Flashcards', err}));
-		// })
-		// .catch(err => res.status(400).json({success: false, message: 'User invalid'}))
+		})
+		.catch(err => res.status(400).json({success: false, message: 'User invalid'}))
+});
+
+// @desc    View a single Flash card by id
+// @route   GET /flashcard/
+router.get('/:fid', ensureAuth, (req, res)=>{
+	User.findById(req.user.id)
+		.then(user => {
+			FlashCard.findById(req.params.fid)
+				.then(flashcard => {
+					if(!flashcard)
+						res.status(200).json({success: false, message: 'You have no Flashcards yet'})
+					res.status(200).json({success: true, message: 'here is flashcards', flashcard});
+				})
+				.catch(err => res.status(500).json({success: false, message: 'Unable to fetch your Flashcard', err}));
+		})
+		.catch(err => res.status(400).json({success: false, message: 'User invalid'}))
 });
 
 // @desc    create Flash card
 // @route   POST /flashcard/
-router.post('/', /*ensureAuth,*/ (req, res) =>{
+router.post('/', ensureAuth,(req, res) =>{
 	const {errors, isValid} = validateFlashcardInput(req.body);
 
 	if(!isValid){
 		res.status(400).json(errors);
 	}else {
-		// User.findById(req.user.id)
-		// 	.then(user => {
+		User.findById(req.user.id)
+			.then(user => {
 				const newFlashcard = new FlashCard({
-					//user: req.user.id,
+					user: req.user.id,
 					subject: req.body.subject,
 					topic: req.body.topic,
 					heading: req.body.heading,
@@ -48,23 +64,23 @@ router.post('/', /*ensureAuth,*/ (req, res) =>{
 				newFlashcard.save()
 					.then(() => res.status(200).json({success: true, message: 'Flashcard Added'}))
 					.catch(err => res.status(500).json({success: false, message: 'Flashcard was not Added', err}))
-			// })
-			// .catch(err => res.status(400).json({success: false, message:'User Invalid'}))
+			})
+			.catch(err => res.status(400).json({success: false, message:'User Invalid'}))
 	};
 });
 
-// @desc    update Flash card
+// @desc    update Flash card by flashcard id
 // @route   POST /flashcard/
-router.post('/:fid', /*ensureAuth,*/ (req, res) =>{
+router.post('/:fid', ensureAuth, (req, res) =>{
 	const {errors, isValid} = validateFlashcardInput(req.body);
 
 	if(!isValid){
 		res.status(400).json(errors);
 	}else {
-		// User.findById(req.user.id)
-		// 	.then(user => {
+		User.findById(req.user.id)
+			.then(user => {
 				const updatedFlashcard = new FlashCard({
-					//user: req.user.id,
+					user: req.user.id,
 					subject: req.body.subject,
 					topic: req.body.topic,
 					heading: req.body.heading,
@@ -75,23 +91,23 @@ router.post('/:fid', /*ensureAuth,*/ (req, res) =>{
 				Flashcard.findOneAndUpdate({user: req.params.fid}, updatedFlashcard, {new: true}).
 					.then((doc) => res.status(200).json({success: true, message: 'Flashcard updated'}))
 					.catch(err => res.status(500).json({success: true, message: 'Unable to update Flashcard'}))
-			// })
-			// .catch(err => res.status(400).json({success: false, message:'User Invalid'}))
+			})
+			.catch(err => res.status(400).json({success: false, message:'User Invalid'}))
 	};
 });
 
-// @desc    delete a Flash card by id
+// @desc    delete a Flash card by flashcard id
 // @route   DELETE /flashcard/:fid
-router.delete('/:fid', /*ensureAuth,*/ (req, res)=>{
-	// User.findById(req.user.id)
-	// 	.then(user => {
+router.delete('/:fid', ensureAuth, (req, res)=>{
+	User.findById(req.user.id)
+		.then(user => {
 			FlashCard.deleteOne({id: req.params.fid})/*{user: req.user.id, id: req.params.fid}*/
 				.then(flashcards => {
 					res.status(200).json({success:true, message:"FlashCard deleted successfully"});
 				})
 				.catch(err => res.status(500).json({success: false, message: 'Unable to delete your Flashcards', err}));
-		// })
-		// .catch(err => res.status(400).json({success: false, message:'User Invalid'}))
+		})
+		.catch(err => res.status(400).json({success: false, message:'User Invalid'}))
 });
 
 
