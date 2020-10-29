bd79fc23438bcf8fec5b76409937e1350b96ae16
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-29 17:37:52 +0530

    Update Flashcard.js

--- models/Flashcard.js
+++ models/Flashcard.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const FlashcardSchema = mongoose.Schema({
-	// user:{
-	// 	type: mongoose.Schema.Types.ObjectId,
-	// 	ref: 'User'
-	// },
+	user:{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
 	subject:{
 		type: String,
 		required: true
