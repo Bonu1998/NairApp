33e99a529115796ed02bb6be4e112c53f6ec72fe
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-29 11:58:13 +0530

    updated dashboard

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -6,17 +6,13 @@ const { ensureAuth, ensureGuest } = require('../../middlewares/auth')
 // @desc    Login/Landing page
 // @route   GET /
 router.get('/', ensureGuest, (req, res) => {
-	// redirect to the main dashboard page
-	// the below json is send for testing purpose
 	res.status(200).json({sucess:true, message:'Landing Page'})
 })
 
 // @desc    Dashboard
 // @route   GET /dashboard
 router.get('/dashboard', ensureAuth, async (req, res) => {
-	// redirect to the main dashboard page
-	// the below json is send for testing purpose
-	res.status(200).json({sucess:true, message:"Sign in Successful"});
+	res.status(200).json({sucess:true, message:"on dashboard"});
 });
 
 module.exports = router;
