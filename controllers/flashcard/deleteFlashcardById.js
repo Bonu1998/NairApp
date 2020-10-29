ee3c3a3ed81cc0016a46952e856e835a86a32269
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-29 17:31:39 +0530

    Add files via upload

---
+++ controllers/flashcard/deleteFlashcardById.js
@@ -0,0 +1,16 @@
+// Load Models
+const User = require('../../models/User');
+const FlashCard = require('../../models/FlashCard');
+
+
+module.exports = (req, res)=>{
+	User.findById(req.user.id)
+		.then(user => {
+			FlashCard.deleteOne({id: req.params.fid})/*{user: req.user.id, id: req.params.fid}*/
+				.then(flashcards => {
+					res.status(200).json({success:true, message:"FlashCard deleted successfully"});
+				})
+				.catch(err => res.status(500).json({success: false, message: 'Unable to delete your Flashcards', err}));
+		})
+		.catch(err => res.status(400).json({success: false, message:'User Invalid'}))
+}
