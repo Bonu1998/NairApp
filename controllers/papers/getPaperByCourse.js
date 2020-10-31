534404e1471470c963e3244470f305320b099667
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-31 16:48:14 +0530

    Add files via upload

---
+++ controllers/papers/getPaperByCourse.js
@@ -0,0 +1,22 @@
+// Load Models
+const User = require('../../models/User');
+const Paper = require('../../models/Paper');
+
+// validation
+const isEmpty = require('../../validation/is-empty');
+
+module.exports = (req, res)=>{
+	User.findById(req.user.id)
+		.then(user => {
+			Paper.find({ course: req.params.pcourse})
+				.then(paper => {
+					if(isEmpty(paper)){
+						res.status(200).json({success: false, message: 'No Papers available'});
+					}else{
+						res.status(200).json({success: true, message: 'here is your Papers', paper});
+					}
+				})
+				.catch(err => res.status(500).json({success: false, message: 'Unable to fetch Papers'}));
+		})
+		.catch(err => res.status(400).json({success: false, message: 'User invalid'}))
+}
