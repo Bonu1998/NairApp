4f8ad2d2b2f301924ad623347ccba68bfcdd5a3e
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-31 15:29:30 +0530

    Add files via upload

---
+++ controllers/questionBank/getAllQuestionBanksBySubject.js
@@ -0,0 +1,22 @@
+// Load Models
+const User = require('../../models/User');
+const QuestionBank = require('../../models/QuestionBank');
+
+// validation
+const isEmpty = require('../../validation/is-empty');
+
+module.exports = (req, res)=>{
+	User.findById(req.user.id)
+		.then(user => {
+			QuestionBank.find({ subject: req.params.qsubject})
+				.then(qb => {
+					if(isEmpty(qb)){
+						res.status(200).json({success: false, message: 'No Question bank available'});
+					}else{
+						res.status(200).json({success: true, message: 'here is your Question bank', qb});
+					}
+				})
+				.catch(err => res.status(500).json({success: false, message: 'Unable to fetch Question Bank'}));
+		})
+		.catch(err => res.status(400).json({success: false, message: 'User invalid'}))
+}
