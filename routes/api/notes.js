d50229a2b199a588423ccb9841ac11e4b99d34c8
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-31 12:13:54 +0530

    Add files via upload

---
+++ routes/api/notes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../../middlewares/auth');
+
+// controllers
+const getAllNotes = require('../../controllers/notes/getAllNotes');
+const getAllNotesById = require('../../controllers/notes/getAllNotesById');
+const addNotes = require('../../controllers/notes/addNotes');
+const updateNotes = require('../../controllers/notes/updateNotes');
+const deleteNotes = require('../../controllers/notes/deleteNotes');
+const { delete } = require('./startPage');
+
+// @desc    View all notes
+// @route   GET /notes/
+router.get('/', ensureAuth, getAllNotes);
+
+// @desc    View particular notes by id
+// @route   GET /notes/:nid
+router.get('/:nid', ensureAuth, getAllNotesById);
+
+// @desc    add notes
+// @route   POST /notes/
+router.post('/', ensureAuth, addNotes);
+
+// @desc    update notes
+// @route   POST /notes/:nid
+router.post('/:nid', ensureAuth, updateNotes);
+
+// @desc    delete notes
+// @route   DELETE /notes/:nid
+router.delete('/:nid', ensureAuth, deleteNotes);
+
+module.exports = router;
