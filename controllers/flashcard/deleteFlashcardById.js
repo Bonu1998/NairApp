c3746bab4121cf8f2b37df17ebf24e019c465531
Bonu1998 <43000788+Bonu1998@users.noreply.github.com>
2020-10-31 12:11:11 +0530

    Update deleteFlashcardById.js

--- controllers/flashcard/deleteFlashcardById.js
+++ controllers/flashcard/deleteFlashcardById.js
@@ -6,7 +6,7 @@ const FlashCard = require('../../models/FlashCard');
 module.exports = (req, res)=>{
 	User.findById(req.user.id)
 		.then(user => {
-			FlashCard.deleteOne({id: req.params.fid})/*{user: req.user.id, id: req.params.fid}*/
+			FlashCard.deleteOne({user: req.user.id, id: req.params.fid})
 				.then(flashcards => {
 					res.status(200).json({success:true, message:"FlashCard deleted successfully"});
 				})
